c1b790fe618d8abd1e30ffcb5e83762d3685e8b9
FredLoney <loneyf@ohsu.edu>
2018-01-11 11:34:24 -0800

    Add PreRanked constants.

---
+++ src/main/java/org/reactome/gsea/config/PreRanked.java
@@ -0,0 +1,8 @@
+package org.reactome.gsea.config;
+
+@SuppressWarnings("unused")
+public final class PreRanked {
+    // Default dataset size parameters lifted from AbstractGseaTool.
+    public static final int DEF_MIN_DATASET_SIZE = 15;
+    public static final int DEF_MAX_DATASET_SIZE = 200;
+}
